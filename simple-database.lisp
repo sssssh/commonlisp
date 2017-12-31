62062b97d75b6f619196cb436b2f08e9a1705b04
Asher Guan <python.sth@gmail.com>
2017-12-31 12:43:52 +0800

    practical database

---
+++ simple-database.lisp
@@ -0,0 +1,114 @@
+(in-package :com.gigamonkeys.simple-db)
+
+(list 1 2 3)
+
+(list :a 1 :b 2 :c 3)
+
+(getf (list :a 1 :b 2 :c 3) :a)
+(getf (list :a 1 :b 2 :c 3) :c)
+
+(defvar *db* nil)
+
+(defun make-cd (title artist rating ripped)
+  (list :title title :artist artist :rating rating :ripped ripped))
+
+(defun add-record (cd) (push cd *db*))
+
+(defun dump-db ()
+  (dolist (cd *db*)
+    (format t "~{~a:~10t~a~%~}~%" cd)))
+
+;;(defun dump-db ()
+;;  (format t "~{~{~a: ~10t~a~%~}~%~}" *db*))
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped [y/n]: ")))
+
+(defun add-cds ()
+  (loop (add-record (prompt-for-cd))
+     (if (not (y-or-n-p "Another? [y/n]: ")) (return))))
+
+(defun save-db (filename)
+  (with-open-file (out filename 
+                   :direction :output
+                   :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
+
+(defun clear-db () (setq *db* nil))
+
+(defun select (selector-fn)
+  (remove-if-not selector-fn *db*))
+
+(remove-if-not #'evenp '(1 2 3 4 5 6 7 8 9 10))
+(remove-if-not #'(lambda (x) (= 0 (mod x 2))) '(1 2 3 4 5 6 7 8 9 10))
+(remove-if-not #'(lambda (x) (= 1 (mod x 2))) '(1 2 3 4 5 6 7 8 9 10))
+(lambda (x) (= 0 (mod x 2)))
+(remove-if-not #'(lambda (cd) (equal (getf cd :artist) "Dixie Chicks")) *db*)
+
+(defun select-by-artist (artist)
+  (remove-if-not
+   #'(lambda (cd) (equal (getf cd :artist) artist))
+   *db*))
+
+(defun artist-selector (artist)
+  #'(lambda (cd) (equal (getf cd :artist) artist)))
+
+(defun foo (a b c)
+  (list a b c))
+
+(defun bar (&key a b c)
+  (list a b c))
+
+(defun spam (&key a (b 20) (c 30 c-p))
+  (list a b c c-p))
+
+(defun where (&key title artist rating (ripped nil ripped-p))
+  #'(lambda (cd)
+      (and
+       (if title     (equal (getf cd :title)  title)   t)
+       (if artist    (equal (getf cd :artist) artist)  t)
+       (if rating    (equal (getf cd :rating) rating)  t)
+       (if ripped-p  (equal (getf cd :ripped) ripped)  t))))
+
+(defmacro where (&rest clauses)
+  `#'(lambda (cd) (and ,@(make-comparisons-list clauses))))
+
+(defun make-comparisons-list (fields)
+  (loop while fields
+     collecting (make-comparison-expr (pop fields) (pop fields))))
+
+(defun make-comparison-expr (field value)
+  `(equal (getf cd ,field) ,value))
+
+
+(defun update (selector-fn &key title artist rating (ripped nil ripped-p))
+  (setf *db*
+        (mapcar 
+         #'(lambda (row)
+             (when (funcall selector-fn row)
+               (if title    (setf (getf row :title) title))
+               (if artist   (setf (getf row :artist) artist))
+               (if rating   (setf (getf row :rating) rating))
+               (if ripped-p (setf (getf row :ripped) ripped)))
+             row) *db*)))
+
+(defun delete-rows (selector-fn)
+  (setf *db* (remove-if selector-fn *db*)))
+
+
+
