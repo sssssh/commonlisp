aade32d387ee557a188b62f65a3d1ab496455e40
Asher Guan <python.sth@gmail.com>
2018-01-05 20:42:40 +0800

    Compression

--- list.lisp
+++ list.lisp
@@ -49,3 +49,47 @@
 (append '(a b) '(c d))
 (append '(a b) '(c d) '(e f))
 (append '(a b) 'c)
+
+
+;; Example:Compression
+;;; run-length encoding
+
+(defun compress (x)
+  (if (consp x)
+      (compr (car x) 1 (cdr x))
+      x))
+
+(defun compr (elt n lst)
+  (if (null lst)
+      (list (n-elts elt n))
+      (let ((next (car lst)))
+	(if (eql next elt)
+	    (compr elt (+ n 1) (cdr lst))
+	    (cons (n-elts elt n)
+		  (compr next 1 (cdr lst)))))))
+
+(defun n-elts (elt n)
+  (if (> n 1)
+      (list n elt)
+      elt))
+
+(compress '(1 1 1 0 1 0 0 0 0 1))
+
+(defun uncompress (lst)
+  (if (null lst)
+      nil
+      (let ((elt (car lst))
+	    (rest (uncompress (cdr lst))))
+	(if (consp elt)
+	    (append (apply #'list-of elt)
+		    rest)
+	    (cons elt rest)))))
+
+(defun list-of (n elt)
+  (if (zerop n)
+      nil
+      (cons elt (list-of (- n 1) elt))))
+
+(uncompress '((3 1) 0 1 (4 0) 1))
+
+(list-of 3 'ho) ; make-list
