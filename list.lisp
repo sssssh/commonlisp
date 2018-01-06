1ebb1091a724a6d7e50612d84a7ad58c1f7cce7f
Asher Guan <python.sth@gmail.com>
2018-01-06 14:09:53 +0800

    Shortest-path

--- list.lisp
+++ list.lisp
@@ -267,3 +267,30 @@
 	 (if (eql key (car pair))
 	     pair
 	     (our-assoc key (cdr alist))))))
+
+
+;; Shortest Path
+;;; (setf min '((a b c) (b c) (c d)))
+
+;;; 最小網路 (minimal network)
+(defun shortest-path (start end net)
+  (bfs end (list (list start)) net))
+
+(defun bfs (end queue net)
+  (if (null queue)
+      nil
+      (let ((path (car queue)))
+	(let ((node (car path)))
+	  (if (eql node end)
+	      (reverse path)
+	      (bfs end
+		   (append (cdr queue)
+			   (new-paths path node net))
+		   net))))))
+
+(defun new-paths (path node net)
+  (mapcar #'(lambda (n)
+	      (cons n path))
+	  (cdr (assoc node net))))
+
+;;; 廣度優先搜索(breadth-first search)
